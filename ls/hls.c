91e221906a41d1b97759853d3dce9357936fc590
popstarmav <cj.johnson2017@gmail.com>
2024-09-07 11:41:03 -0500

    fix betty doc style

--- ls/hls.c
+++ ls/hls.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <dirent.h>
 
+/**
+ * main - Enrty point for program
+ * lists the contents of the current direcroty
+ * similar to the 'ls' command
+ * 
+ * Return: 0 on success, 1 on failure
+ */
 int main(void)
 {
     struct dirent *entry;
