021fae6e2cf823cdc3f5fdf5a11cc119d384fd55
popstarmav <cj.johnson2017@gmail.com>
2024-09-07 11:26:39 -0500

    Add hls.c for directory listing

---
+++ ls/hls.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(void)
+{
+    struct dirent *entry;
+    DIR *dir = opendir(".");
+
+    if (dir == NULL)
+    {
+        perror("opendir");
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (entry->d_name[0] != '.')  // Skipping hidden files
+        {
+            printf("%s  ", entry->d_name);
+        }
+    }
+
+    printf("\n");
+    closedir(dir);
+    return 0;
+}
