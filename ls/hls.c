143a425cf3f15f4fda1cc1fa709faf2347864ae9
popstarmav <cj.johnson2017@gmail.com>
2024-09-08 01:04:53 -0500

    Add support for listing files and directories

--- ls/hls.c
+++ ls/hls.c
@@ -1,33 +1,71 @@
+#include "hls.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+#include <errno.h>
+#include <string.h>
 
 /**
- * main - Enrty point for program
- * lists the contents of the current direcroty
- * similar to the 'ls' command
- * 
- * Return: 0 on success, 1 on failure
+ * list_directory - Lists contents of a directory or file
+ * @path: Directory or file path to list
+ * @program_name: Name of the program (argv[0])
  */
-int main(void)
+void list_directory(const char *path, const char *program_name)
 {
     struct dirent *entry;
-    DIR *dir = opendir(".");
+    DIR *dir = opendir(path);
 
     if (dir == NULL)
     {
-        perror("opendir");
-        return 1;
+        // Error handling for files and directories
+        FILE *fp = fopen(path, "r");
+        if (fp == NULL)
+        {
+            // Print error to stderr
+            fprintf(stderr, "%s: cannot access '%s': %s\n", program_name, path, strerror(errno));
+            exit(EXIT_FAILURE);  // Non-zero exit on error
+        }
+        else
+        {
+            printf("%s\n", path);  // If it's a file, print the filename
+            fclose(fp);
+        }
+        return;
     }
 
+    printf("%s:\n", path);  // Print directory name
     while ((entry = readdir(dir)) != NULL)
     {
-        if (entry->d_name[0] != '.')
+        if (entry->d_name[0] != '.')  // Skip hidden files
         {
             printf("%s  ", entry->d_name);
         }
     }
 
-    printf("\n");
+    printf("\n\n");
     closedir(dir);
-    return 0;
+}
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector (file/directory names)
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        list_directory(".", argv[0]);  // Default to current directory if no arguments
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            list_directory(argv[i], argv[0]);
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
---
+++ ls/hls.h
@@ -0,0 +1,10 @@
+#ifndef HSL_H
+#define HSL_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <dirent.h>
+
+void list_dir(const char *path, const char *program_name);
+
+#endif /* HSL_H */
