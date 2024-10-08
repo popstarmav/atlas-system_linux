e90ceaa94b4c6c2ae099f9087436eac0e25d3f46
popstarmav <cj.johnson2017@gmail.com>
2024-10-08 09:22:22 -0500

    update file

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -22,18 +22,44 @@ def update_mem_file(pid, search_string, replace_string, heap_start, heap_stop):
         f.seek(heap_start)
         data = f.read(heap_stop - heap_start)
         string_offset = data.find(search_string.encode())
+        
         if string_offset > -1:
-            f.seek(heap_start + string_offset)
-            f.write(replace_string.encode())
-            return True  # Indicate success
-    return False  # Indicate failure
+            if len(search_string) == len(replace_string):
+                # Normal replace
+                f.seek(heap_start + string_offset)
+                f.write(replace_string.encode())
+                return "SUCCESS!"  # Normal replace
+            
+            elif len(search_string) > len(replace_string):
+                # Short replace
+                f.seek(heap_start + string_offset)
+                f.write(replace_string.encode())
+                return "SUCCESS! (Short replace)"  # Short replace
+
+            elif len(search_string) < len(replace_string):
+                # Replace by shortest string
+                f.seek(heap_start + string_offset)
+                f.write(replace_string.encode())
+                return "SUCCESS! (Replaced by longer string)"  # Replace by longest string
+        else:
+            # Not found case
+            return "String not found."
+
+    # If replace string is empty
+    if not replace_string:
+        return "SUCCESS! (Replaced by empty string)"  # Replace by empty string
 
 if __name__ == "__main__":
-    if len(argv) != 4 or len(argv[2]) != len(argv[3]):
+    if len(argv) != 4:
         print(USAGE)
         exit(1)
 
-    heap_start, heap_stop = parse_maps_file(int(argv[1]))
-    if update_mem_file(int(argv[1]), argv[2], argv[3], heap_start, heap_stop):
-        print("SUCCESS!")
+    pid = int(argv[1])
+    search_string = argv[2]
+    replace_string = argv[3]
+
+    heap_start, heap_stop = parse_maps_file(pid)
+    result = update_mem_file(pid, search_string, replace_string, heap_start, heap_stop)
+    
+    print(result)
 
