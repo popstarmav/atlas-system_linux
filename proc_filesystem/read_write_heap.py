9fc5aa02f4ed7f54aa02f5319d047b28948cc789
popstarmav <cj.johnson2017@gmail.com>
2024-10-08 09:45:17 -0500

    update file

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -1,53 +1,36 @@
 #!/usr/bin/python3
 """Finds & overwrites a string in a process' mem file"""
 
-from sys import argv
+from sys import argv, exit
 
 USAGE = "USAGE: read_write_heap.py pid search_string replace_string"
 
 def parse_maps_file(pid):
-    """Parses /proc/PID/maps file for heap info"""
-    heap_start = heap_stop = None
-    with open(f"/proc/{pid}/maps", "r") as file:
-        for line in file:
-            if "[heap]" in line:
-                heap_start, heap_stop = [int(x, 16) for x in line.split(" ")[0].split("-")]
-    if heap_start is None or heap_stop is None:
+    """Parses /proc/PID/maps file for heap info."""
+    try:
+        with open(f"/proc/{pid}/maps", "r") as file:
+            for line in file:
+                if "[heap]" in line:
+                    heap_start, heap_stop = [int(x, 16) for x in line.split()[0].split("-")]
+                    return heap_start, heap_stop
+    except Exception:
         exit(1)
-    return heap_start, heap_stop
+    exit(1)
 
 def update_mem_file(pid, search_string, replace_string, heap_start, heap_stop):
-    """Finds search_string in /proc/PID/mem and writes replace_string"""
-    with open(f"/proc/{pid}/mem", "r+b") as f:
-        f.seek(heap_start)
-        data = f.read(heap_stop - heap_start)
-        string_offset = data.find(search_string.encode())
-        
-        if string_offset > -1:
-            if len(search_string) == len(replace_string):
-                # Normal replace
-                f.seek(heap_start + string_offset)
-                f.write(replace_string.encode())
-                return "SUCCESS!"  # Normal replace
-            
-            elif len(search_string) > len(replace_string):
-                # Short replace
-                f.seek(heap_start + string_offset)
-                f.write(replace_string.encode())
-                return "SUCCESS! (Short replace)"  # Short replace
-
-            elif len(search_string) < len(replace_string):
-                # Replace by shortest string
-                f.seek(heap_start + string_offset)
-                f.write(replace_string.encode())
-                return "SUCCESS! (Replaced by longer string)"  # Replace by longest string
-        else:
-            # Not found case
-            return "String not found."
-
-    # If replace string is empty
-    if not replace_string:
-        return "SUCCESS! (Replaced by empty string)"  # Replace by empty string
+    """Finds search_string in /proc/PID/mem and writes replace_string."""
+    try:
+        with open(f"/proc/{pid}/mem", "r+b") as mem_file:
+            mem_file.seek(heap_start)
+            data = mem_file.read(heap_stop - heap_start)
+            string_offset = data.find(search_string.encode())
+            if string_offset != -1:
+                mem_file.seek(heap_start + string_offset)
+                mem_file.write(replace_string.encode())
+                return True
+    except Exception:
+        return False
+    return False
 
 if __name__ == "__main__":
     if len(argv) != 4:
@@ -58,8 +41,13 @@ if __name__ == "__main__":
     search_string = argv[2]
     replace_string = argv[3]
 
+    if len(search_string) < len(replace_string):
+        print(USAGE)
+        exit(1)
+
     heap_start, heap_stop = parse_maps_file(pid)
-    result = update_mem_file(pid, search_string, replace_string, heap_start, heap_stop)
-    
-    print(result)
+    if update_mem_file(pid, search_string, replace_string, heap_start, heap_stop):
+        print("SUCCESS!")
+    else:
+        exit(1)
 
