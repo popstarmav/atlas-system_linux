964f233804d023d2cdf6beaa317ce40ca5377a54
popstarmav <cj.johnson2017@gmail.com>
2024-10-16 17:44:53 -0500

    elf file rns correclty but cant find elf files

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -16,46 +16,46 @@ void print_magic(unsigned char *e_ident);
  */
 int main(int argc, char **argv)
 {
-    int fd;
-    ssize_t read_size;
-    Elf64_Ehdr header;
+	int fd;
+	ssize_t read_size;
+	Elf64_Ehdr header;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        return (1);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (1);
+	}
 
-    /* Open the ELF file */
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Error: Cannot open file");
-        return (1);
-    }
+	/* Open the ELF file */
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error: Cannot open file");
+		return (1);
+	}
 
-    /* Read ELF header */
-    read_size = read(fd, &header, sizeof(header));
-    if (read_size != sizeof(header))
-    {
-        perror("Error: Cannot read ELF header");
-        close(fd);
-        return (1);
-    }
+	/* Read ELF header */
+	read_size = read(fd, &header, sizeof(header));
+	if (read_size != sizeof(header))
+	{
+		perror("Error: Cannot read ELF header");
+		close(fd);
+		return (1);
+	}
 
-    /* Verify if it's an ELF file by checking magic bytes */
-    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
-    {
-        fprintf(stderr, "Error: Not an ELF file\n");
-        close(fd);
-        return (1);
-    }
+	/* Verify if it's an ELF file by checking magic bytes */
+	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		fprintf(stderr, "Error: Not an ELF file\n");
+		close(fd);
+		return (1);
+	}
 
-    /* Print ELF header */
-    print_elf_header(&header);
+	/* Print ELF header */
+	print_elf_header(&header);
 
-    close(fd);
-    return (0);
+	close(fd);
+	return (0);
 }
 
 /**
@@ -64,40 +64,46 @@ int main(int argc, char **argv)
  */
 void print_elf_header(Elf64_Ehdr *header)
 {
-    printf("ELF Header:\n");
-    print_magic(header->e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
 
-    printf("  Class:                             %s\n",
-           header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
-           header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Invalid class");
+	printf("  Class:                             %s\n",
+		header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
+		header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Invalid class");
 
-    printf("  Data:                              %s\n",
-           header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-           header->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
+	printf("  Data:                              %s\n",
+		header->e_ident[EI_DATA] == ELFDATA2LSB ?
+		"2's complement, little endian" :
+		header->e_ident[EI_DATA] == ELFDATA2MSB ?
+		"2's complement, big endian" :
+		"Invalid data encoding");
 
-    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  Version:                           %d (current)\n",
+		header->e_ident[EI_VERSION]);
 
-    printf("  OS/ABI:                            %s\n",
-           header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
-           header->e_ident[EI_OSABI] == ELFOSABI_HPUX ? "HP-UX" :
-           header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "NetBSD" :
-           header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
-           "Unknown");
+	printf("  OS/ABI:                            %s\n",
+		header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
+		header->e_ident[EI_OSABI] == ELFOSABI_HPUX ? "HP-UX" :
+		header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "NetBSD" :
+		header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
+		"Unknown");
 
-    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-    printf("  Type:                              %d\n", header->e_type);
-    printf("  Machine:                           %d\n", header->e_machine);
-    printf("  Version:                           0x%x\n", header->e_version);
-    printf("  Entry point address:               0x%lx\n", header->e_entry);
-    printf("  Start of program headers:          %ld (bytes into file)\n", header->e_phoff);
-    printf("  Start of section headers:          %ld (bytes into file)\n", header->e_shoff);
-    printf("  Flags:                             0x%x\n", header->e_flags);
-    printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
-    printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
-    printf("  Number of program headers:         %d\n", header->e_phnum);
-    printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
-    printf("  Number of section headers:         %d\n", header->e_shnum);
-    printf("  Section header string table index: %d\n", header->e_shstrndx);
+	printf("  ABI Version:%d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %d\n", header->e_type);
+	printf("  Machine:                           %d\n", header->e_machine);
+	printf("  Version:                           0x%x\n", header->e_version);
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
+	printf("  Start of program headers:          %ld (bytes into file)\n",
+		header->e_phoff);
+	printf("  Start of section headers:          %ld (bytes into file)\n",
+		header->e_shoff);
+	printf("  Flags:                             0x%x\n", header->e_flags);
+	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
+	printf("  Size of program headers:%d (bytes)\n", header->e_phentsize);
+	printf("  Number of program headers:         %d\n", header->e_phnum);
+	printf("  Size of section headers:%d (bytes)\n", header->e_shentsize);
+	printf("  Number of section headers:         %d\n", header->e_shnum);
+	printf("  Section header string table index: %d\n", header->e_shstrndx);
 }
 
 /**
@@ -106,11 +112,11 @@ void print_elf_header(Elf64_Ehdr *header)
  */
 void print_magic(unsigned char *e_ident)
 {
-    int i;
+	int i;
 
-    printf("  Magic:   ");
-    for (i = 0; i < EI_NIDENT; i++)
-        printf("%02x ", e_ident[i]);
-    printf("\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", e_ident[i]);
+	printf("\n");
 }
 
