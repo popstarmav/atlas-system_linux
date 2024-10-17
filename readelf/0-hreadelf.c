fc71357fcd45087958378d50e7599eec467cab98
popstarmav <cj.johnson2017@gmail.com>
2024-10-17 15:13:47 +0000

    fixing logic erros

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -67,10 +67,12 @@ void print_elf_header(Elf64_Ehdr *header)
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 
+	/* Class */
 	printf("  Class:                             %s\n",
 		header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
 		header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Invalid class");
 
+	/* Data */
 	printf("  Data:                              %s\n",
 		header->e_ident[EI_DATA] == ELFDATA2LSB ?
 		"2's complement, little endian" :
@@ -78,9 +80,11 @@ void print_elf_header(Elf64_Ehdr *header)
 		"2's complement, big endian" :
 		"Invalid data encoding");
 
+	/* Version */
 	printf("  Version:                           %d (current)\n",
 		header->e_ident[EI_VERSION]);
 
+	/* OS/ABI */
 	printf("  OS/ABI:                            %s\n",
 		header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
 		header->e_ident[EI_OSABI] == ELFOSABI_HPUX ? "HP-UX" :
@@ -88,21 +92,52 @@ void print_elf_header(Elf64_Ehdr *header)
 		header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
 		"Unknown");
 
-	printf("  ABI Version:         %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %d\n", header->e_type);
-	printf("  Machine:                           %d\n", header->e_machine);
-	printf("  Version:                           0x%x\n", header->e_version);
-	printf("  Entry point address:               0x%lx\n", header->e_entry);
-	printf("  Start of program headers:          %ld (bytes into file)\n",
-		header->e_phoff);
-	printf("  Start of section headers:          %ld (bytes into file)\n",
-		header->e_shoff);
+	/* ABI Version */
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+
+	/* Type */
+	printf("  Type:                              %s\n",
+		header->e_type == ET_EXEC ? "EXEC (Executable file)" :
+		header->e_type == ET_REL ? "REL (Relocatable file)" :
+		header->e_type == ET_DYN ? "DYN (Shared object file)" :
+		header->e_type == ET_CORE ? "CORE (Core file)" : "Unknown");
+
+	/* Machine */
+	printf("  Machine:                           %s\n",
+		header->e_machine == EM_386 ? "Intel 80386" :
+		header->e_machine == EM_X86_64 ? "AMD x86-64" : "Unknown");
+
+	/* Entry point address */
+	if (header->e_ident[EI_CLASS] == ELFCLASS32)
+		printf("  Entry point address:               0x%x\n", (unsigned int)header->e_entry);
+	else
+		printf("  Entry point address:               0x%lx\n", header->e_entry);
+
+	/* Start of program headers */
+	printf("  Start of program headers:          %ld (bytes into file)\n", header->e_phoff);
+
+	/* Start of section headers */
+	printf("  Start of section headers:          %ld (bytes into file)\n", header->e_shoff);
+
+	/* Flags */
 	printf("  Flags:                             0x%x\n", header->e_flags);
+
+	/* Size of ELF header */
 	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
-	printf("  Size of program headers:       %d (bytes)\n", header->e_phentsize);
+
+	/* Size of program headers */
+	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
+
+	/* Number of program headers */
 	printf("  Number of program headers:         %d\n", header->e_phnum);
-	printf("  Size of section headers:       %d (bytes)\n", header->e_shentsize);
+
+	/* Size of section headers */
+	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
+
+	/* Number of section headers */
 	printf("  Number of section headers:         %d\n", header->e_shnum);
+
+	/* Section header string table index */
 	printf("  Section header string table index: %d\n", header->e_shstrndx);
 }
 
@@ -119,4 +154,5 @@ void print_magic(unsigned char *e_ident)
 		printf("%02x ", e_ident[i]);
 	printf("\n");
 }
+
 
