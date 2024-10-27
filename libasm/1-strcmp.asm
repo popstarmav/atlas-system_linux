fefdeba548065a93e1a959248d1a2ff8ccf3fa87
popstarmav <cj.johnson2017@gmail.com>
2024-10-27 12:07:59 -0500

    Fix asm_strcmp to handle null terminators and return correct strcmp values (1, -1, 0).

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -9,31 +9,31 @@ asm_strcmp:
     mov bl, byte [rsi]         ; Load byte from s2
 
     test al, al                ; Check if *s1 is null
-    jz .end_of_strings         
+    jz .end_of_strings 
     test bl, bl                ; Check if *s2 is null
-    jz .end_of_strings         
+    jz .end_of_strings
 
-    cmp al, bl                 
-    jne .not_equal             
+    cmp al, bl
+    jne .not_equal
 
-    inc rdi                    
-    inc rsi                    
-    jmp .compare_loop          
+    inc rdi             
+    inc rsi
+    jmp .compare_loop
 
 .end_of_strings:
-    cmp al, bl                 
-    je .return_equal           
+    cmp al, bl          
+    je .return_equal
 
 .not_equal:
     mov eax, 1                 ; Set default return to 1 or -1
-    cmp al, bl                 
-    jl .less_than              
+    cmp al, bl
+    jl .less_than
 
     ret                        ; Return 1 if `al > bl`
 
 .less_than:
     mov eax, -1                ; Return -1 if `al < bl`
-    ret                       
+    ret
 
 .return_equal:
     xor eax, eax               ; Set eax to 0 for equality
