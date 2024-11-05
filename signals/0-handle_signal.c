6d392f1704e2a1ea1529c4c85940ef7f9a9bb1be
popstarmav <cj.johnson2017@gmail.com>
2024-11-05 11:24:57 -0600

    Add signal handler function and header file for SIGINT handling

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <signal.h>
 
+/**
+ * sigint_handler - Prints a message when SIGINT (Ctrl+C) is recived
+ * @signnum: the signal number
+ */
 void sigint_handler(int signum)
 {
 	printf("Gotcha! [%d]\n", signum);
 	fflush(stdout);
 }
 
+/**
+ * handle_signal - Sets sigint_handler to handle SIGINT (CTRL+C)
+ *
+ * Return: 0 on success, -1 on failure
+ */
 int handle_signal(void)
 {
 	if (signal(SIGINT, sigint_handler) == SIG_ERR)
