85f74e2fdd4f1d5dfe5a008d4a128355033386b1
popstarmav <cj.johnson2017@gmail.com>
2024-11-05 13:39:53 -0600

    Define handler for SIGQUIT signal to print sender's PID

--- signals/signals.h
+++ signals/signals.h
@@ -4,6 +4,7 @@
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
+int trace_signal_sender(void);
 void (*current_handler_sigaction(void))(int);
 
 #endif /* SIGNALS_H */
