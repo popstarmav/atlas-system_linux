a16655fc6735f026d66c259a97c6b472055bc337
popstarmav <cj.johnson2017@gmail.com>
2024-11-05 13:21:49 -0600

    Fix unused parameter warning in print_hello function

--- signals/signals.h
+++ signals/signals.h
@@ -4,7 +4,7 @@
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
- void (*current_handler_sigaction(void))(int);
+void (*current_handler_sigaction(void))(int);
 
 #endif /* SIGNALS_H */
 
