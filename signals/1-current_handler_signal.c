05d7eea0a76bbaa7d2a4f8792ed23c850bc748f2
popstarmav <cj.johnson2017@gmail.com>
2024-11-05 11:46:38 -0600

    Add signal handler setup for SIGINT with custom print_hello handler

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -6,19 +6,19 @@
  *
  * Return: Pointer to the current handler function, or NULL on failure
  */
-void (*current_handler_signal(void))(int) {
-    void (*current_handler)(int);
+void (*current_handler_signal(void))(int)
+{
+	void (*current_handler)(int);
 
-    // Retrieve the current handler for SIGINT without changing it
-    current_handler = signal(SIGINT, SIG_IGN);
+	current_handler = signal(SIGINT, SIG_IGN);
 
-    if (current_handler == SIG_ERR) {
-        return NULL; // Return NULL if retrieving the handler fails
-    }
+	if (current_handler == SIG_ERR)
+	{
+		return (NULL);
+	}
 
-    // Restore the original handler
-    signal(SIGINT, current_handler);
+	signal(SIGINT, current_handler);
 
-    return current_handler;
+	return (current_handler);
 }
 
