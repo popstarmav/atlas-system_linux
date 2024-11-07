3799405cab8ac6b8396d762b63b5f0a94d9b735b
popstarmav <cj.johnson2017@gmail.com>
2024-11-06 20:56:49 -0600

    fix betty errors

--- signals/7-signal_send.c
+++ signals/7-signal_send.c
@@ -11,20 +11,20 @@
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <pid>\n", argv[0]);
-        return (EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <pid>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
 
-    pid_t pid = atoi(argv[1]);
+	pid_t pid = atoi(argv[1]);
 
-    if (kill(pid, SIGINT) == -1)
-    {
-        perror("Error sending signal");
-        return (EXIT_FAILURE);
-    }
+	if (kill(pid, SIGINT) == -1)
+	{
+		perror("Error sending signal");
+		return (EXIT_FAILURE);
+	}
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
