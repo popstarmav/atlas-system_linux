e08513a87be3c57a5a2e6b499f02286cf5c91e7a
popstarmav <cj.johnson2017@gmail.com>
2024-11-06 20:48:43 -0600

    Fix signal handling to match output format for known and unknown signals

--- signals/5-signal_describe.c
+++ signals/5-signal_describe.c
@@ -3,6 +3,13 @@
 #include <signal.h>
 #include <string.h>
 
+/**
+ * main - Prints the description of a signal by its number.
+ * @argc: Argument count.
+ * @argv: Argument vector containing the signal number.
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on error.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -10,24 +17,25 @@ int main(int argc, char *argv[])
 		printf("Usage: %s <signum>\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
-	
+
 	int signum = atoi(argv[1]);
-	
+
 	if (signum == 0)
 	{
 		printf("0: Unknown signal 0\n");
 		return (EXIT_SUCCESS);
 	}
-	
+
 	if (signum <= 0 || signum > 100)
 	{
 		printf("Invalid signal number\n");
 		return (EXIT_FAILURE);
 	}
-	
+
 	const char *signal_description = strsignal(signum);
+
 	printf("%d: %s\n", signum, signal_description);
-	
+
 	return (EXIT_SUCCESS);
 }
 
