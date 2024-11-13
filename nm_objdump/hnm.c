80ec7a4dba60779553f693e85e5e80af91430969
popstarmav <cj.johnson2017@gmail.com>
2024-11-13 14:45:31 -0600

    Implement ELF file symbol display in hnm program

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -1,45 +1,77 @@
-#include "hnm.h"
+#include <libelf.h>
+#include <gelf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    print_symbols("your_elf_file_path");
-    return 0;
-}
-
-void print_symbols(const char *filename) {
-    int fd = open("file_path", O_RDONLY);
-    Elf *elf;
-    size_t shstrndx;
-
-    if (elf_version(EV_CURRENT) == EV_NONE) {
-        fprintf(stderr, "ELF library initialization failed: %s\n", elf_errmsg(-1));
-        exit(EXIT_FAILURE);
-    }
-
-    fd = open(filename, O_RDONLY);
+void display_symbols(const char *filename) {
+    int fd = open(filename, O_RDONLY);
     if (fd < 0) {
         perror("open");
         exit(EXIT_FAILURE);
     }
 
-    elf = elf_begin(fd, ELF_C_READ, NULL);
-    if (!elf) {
-        fprintf(stderr, "elf_begin() failed: %s.\n", elf_errmsg(-1));
+    if (elf_version(EV_CURRENT) == EV_NONE) {
+        fprintf(stderr, "ELF library initialization failed.\n");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    if (elf_getshdrstrndx(elf, &shstrndx) != 0) {
-        fprintf(stderr, "elf_getshdrstrndx() failed: %s\n", elf_errmsg(-1));
-        elf_end(elf);
+    Elf *elf = elf_begin(fd, ELF_C_READ, NULL);
+    if (!elf) {
+        fprintf(stderr, "Failed to read ELF file.\n");
         close(fd);
         exit(EXIT_FAILURE);
+    }
+
+    Elf_Scn *scn = NULL;
+    GElf_Shdr shdr;
+    while ((scn = elf_nextscn(elf, scn)) != NULL) {
+        if (gelf_getshdr(scn, &shdr) != &shdr) {
+            fprintf(stderr, "Failed to get section header.\n");
+            exit(EXIT_FAILURE);
+        }
+        
+        if (shdr.sh_type == SHT_SYMTAB) {
+            Elf_Data *data = elf_getdata(scn, NULL);
+            int symbol_count = shdr.sh_size / shdr.sh_entsize;
+            for (int i = 0; i < symbol_count; ++i) {
+                GElf_Sym sym;
+                gelf_getsym(data, i, &sym);
+
+                const char *name = elf_strptr(elf, shdr.sh_link, sym.st_name);
+                char type = '?';
+
+                switch (GELF_ST_TYPE(sym.st_info)) {
+                    case STT_OBJECT: type = 'D'; break;
+                    case STT_FUNC:   type = 'T'; break;
+                    case STT_SECTION:type = 'S'; break;
+                    case STT_FILE:   type = 'F'; break;
+                    default:         type = '?'; break;
+                }
+
+                printf("%016lx %c %s\n", (long) sym.st_value, type, name ? name : "");
+            }
+        }
     }
 
     elf_end(elf);
     close(fd);
 }
 
-char get_symbol_type(GElf_Shdr shdr, GElf_Sym sym) {
-    return 'T';
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [objfile...]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        printf("\n%s:\n", argv[i]);
+        display_symbols(argv[i]);
+    }
+
+    return EXIT_SUCCESS;
 }
 
--- nm_objdump/hnm.h
+++ nm_objdump/hnm.h
@@ -1,14 +1,6 @@
 #ifndef HNM_H
 #define HNM_H
 
-#include <libelf.h>
-#include <gelf.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-void print_symbols(const char *filename);
-char get_symbol_type(GElf_Shdr shdr, GElf_Sym sym);
+void display_symbols(const char *filename);
 
 #endif
