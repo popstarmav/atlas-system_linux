10b37e5c5e3dce0147664644df74af86f6404f82
popstarmav <cj.johnson2017@gmail.com>
2024-11-13 12:26:43 -0600

    test

--- nm_objdump/hnm.h
+++ nm_objdump/hnm.h
@@ -3,15 +3,12 @@
 
 #include <libelf.h>
 #include <gelf.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>  // For close()
+#include <fcntl.h>
+#include <unistd.h>
 
-// Function prototypes
 void print_symbols(const char *filename);
 char get_symbol_type(GElf_Shdr shdr, GElf_Sym sym);
 
-#endif /* HNM_H */
-
+#endif
