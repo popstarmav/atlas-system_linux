8e101c1f4d15c0dadfcf94997ea7add7d1025069
popstarmav <cj.johnson2017@gmail.com>
2024-11-14 01:37:38 -0600

    function that tests if a process exists

--- signals/signals.h
+++ signals/signals.h
@@ -5,6 +5,7 @@ int handle_signal(void);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
 int trace_signal_sender(void);
+int pid_exist(pid_t pid);
 void (*current_handler_sigaction(void))(int);
 
 #endif /* SIGNALS_H */
