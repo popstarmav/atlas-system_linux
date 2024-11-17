829e4ab7ca24622e6f773c02c4d47aece9aff5a4
popstarmav <cj.johnson2017@gmail.com>
2024-11-17 14:41:23 -0600

    Initial implementation of pid_exist function

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 /**
- * pid_exist - Checks if a process with the given PID exists.
- * @pid: The process ID to check.
+ * pid_exist - Checks if a process with the given PID exists
+ * @pid: The process ID to check
  *
- * Return: 1 if the process exists, 0 otherwise.
+ * Return: 1 if the process exists, 0 otherwise
  */
 int pid_exist(pid_t pid)
 {
