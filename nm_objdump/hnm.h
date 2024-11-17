fe20fa9a871c6108ecc47665b450ed70ed3a4407
popstarmav <cj.johnson2017@gmail.com>
2024-11-17 15:19:54 -0600

    test push

--- nm_objdump/hnm.h
+++ nm_objdump/hnm.h
@@ -1,6 +1,9 @@
 #ifndef HNM_H
 #define HNM_H
+#include  <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void display_symbols(const char *filename);
+int process_file(char *file_name, int multiple, char **argv);
 
 #endif
