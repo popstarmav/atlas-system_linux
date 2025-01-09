e64559ff71414820d1086addff5cd14a49d9de3f
popstarmav <cj.johnson2017@gmail.com>
2025-01-09 13:07:49 -0600

    strace

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -6,41 +6,52 @@
 #include <unistd.h>
 #include <sys/user.h>
 
+/**
+ * Program to trace system calls made by a given command.
+ * Usage: ./strace_0 command [args...]
+ */
 int main(int argc, char *argv[])
 {
-    pid_t child;
-    int status;
-    struct user_regs_struct regs;
+    pid_t child;                   // Process ID of the child
+    int status;                    // Status for wait()
+    struct user_regs_struct regs;  // Structure to store registers
 
+    // Ensure the program is called with at least one argument
     if (argc < 2) {
-        printf("Usage: %s command [args...]\n", argv[0]);
+        fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
         return 1;
     }
 
-    child = fork();
+    child = fork(); // Fork the process
     if (child == 0) {
+        // Child process: Enable tracing and execute the given command
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execv(argv[1], argv + 1);
+        perror("execv"); // If execv fails, print an error
         exit(1);
+    } else if (child > 0) {
+        // Parent process: Trace the child process
+        while (1) {
+            // Wait for child state change
+            wait(&status);
+
+            // Break if the child process exits
+            if (WIFEXITED(status)) {
+                break;
+            }
+
+            // Get the syscall number (orig_rax) from the child process
+            ptrace(PTRACE_GETREGS, child, NULL, &regs);
+            printf("%lld\n", (long long)regs.orig_rax);
+
+            // Continue to the next syscall entry
+            ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+        }
+    } else {
+        // Fork failed
+        perror("fork");
+        return 1;
     }
 
-    wait(&status);
-    while (1) {
-        ptrace(PTRACE_SYSCALL, child, NULL, NULL);
-        wait(&status);
-        
-        if (WIFEXITED(status))
-            break;
-            
-        ptrace(PTRACE_GETREGS, child, NULL, &regs);
-        printf("%lld\n", (long long)regs.orig_rax);
-        
-        ptrace(PTRACE_SYSCALL, child, NULL, NULL);
-        wait(&status);
-        
-        if (WIFEXITED(status))
-            break;
-    }
     return 0;
 }
-
