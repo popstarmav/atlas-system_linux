95f8349ad0c13ae824cb660bbe55b11eb766b4fb
popstarmav <cj.johnson2017@gmail.com>
2025-01-09 00:07:18 +0000

    strace

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/user.h>
 #include <sys/reg.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,37 +15,49 @@ int main(int argc, char *argv[])
     struct user_regs_struct regs;
 
     if (argc < 2) {
-        printf("Usage: %s command [args...]\n", argv[0]);
+        fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
         return 1;
     }
 
     child = fork();
-    if (child == 0) {
-        /* Child process */
-        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execv(argv[1], argv + 1);
-        exit(1);
+    if (child == -1) {
+        perror("fork");
+        return 1;
     }
-    else {
-        /* Parent process */
-        while (1) {
-            wait(&status);
-            if (WIFEXITED(status))
-                break;
 
-            ptrace(PTRACE_GETREGS, child, NULL, &regs);
-            
-            /* Print syscall number */
+    if (child == 0) {
+        /* Child process: request to be traced and execute the command */
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
+            perror("ptrace(PTRACE_TRACEME)");
+            exit(1);
+        }
+        execv(argv[1], argv + 1);
+        perror("execv");
+        exit(1);
+    } else {
+        /* Parent process: trace the child process */
+        while (1) {
+            if (waitpid(child, &status, 0) == -1) {
+                perror("waitpid");
+                return 1;
+            }
+            if (WIFEXITED(status)) {
+                break;
+            }
+
+            /* Retrieve the system call number */
+            if (ptrace(PTRACE_GETREGS, child, NULL, &regs) == -1) {
+                perror("ptrace(PTRACE_GETREGS)");
+                return 1;
+            }
+
             printf("%lld\n", (long long)regs.orig_rax);
 
-            /* Continue to next syscall */
-            ptrace(PTRACE_SYSCALL, child, NULL, NULL);
-            wait(&status);
-            if (WIFEXITED(status))
-                break;
-            
-            /* Continue to next syscall entry */
-            ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+            /* Continue to the next system call */
+            if (ptrace(PTRACE_SYSCALL, child, NULL, NULL) == -1) {
+                perror("ptrace(PTRACE_SYSCALL)");
+                return 1;
+            }
         }
     }
 
