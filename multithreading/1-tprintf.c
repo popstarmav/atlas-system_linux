98d8c206bb2fb28df61c64e288a8a27cea8b2e6f
popstarmav <cj.johnson2017@gmail.com>
2025-01-10 14:33:51 -0600

    print out a given formatted string.

--- multithreading/1-tprintf.c
+++ multithreading/1-tprintf.c
@@ -11,13 +11,13 @@ int tprintf(char const *format, ...)
 {
 	va_list args;
 	int printed_chars;
-	
+
 	printed_chars = printf("[%lu] ", pthread_self());
-	
+
 	va_start(args, format);
 	printed_chars += vprintf(format, args);
 	va_end(args);
-	
+
 	return (printed_chars);
 }
 
