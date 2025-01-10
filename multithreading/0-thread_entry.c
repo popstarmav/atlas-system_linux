6d2d0d0854ce10cdc8f2b57453537224fc6f3ec1
popstarmav <cj.johnson2017@gmail.com>
2025-01-10 07:53:08 -0600

    point to a new thread.

---
+++ multithreading/0-thread_entry.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "multithreading.h"
+
+/**
+ * thread_entry - Entry point for a new thread
+ * @arg: Pointer to a string to be printed
+ *
+ * Return: NULL
+ */
+void *thread_entry(void *arg)
+{
+    printf("%s\n", (char *)arg);
+    pthread_exit(NULL);
+}
+
