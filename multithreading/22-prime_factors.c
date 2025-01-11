d5a07ba98238d6156c0e5838bf288e92ef5b61d9
popstarmav <cj.johnson2017@gmail.com>
2025-01-10 19:40:23 -0600

    tread pool will have for purpose to execute a list of tasks.

--- multithreading/22-prime_factors.c
+++ multithreading/22-prime_factors.c
@@ -9,18 +9,18 @@
  */
 task_t *create_task(task_entry_t entry, void *param)
 {
-    task_t *task = malloc(sizeof(task_t));
-
-    if (!task)
-        return NULL;
-
-    task->entry = entry;
-    task->param = param;
-    task->status = PENDING;
-    task->result = NULL;
-    pthread_mutex_init(&task->lock, NULL);
-
-    return task;
+	task_t *task = malloc(sizeof(task_t));
+	
+	if (!task)
+		return (NULL);
+	
+	task->entry = entry;
+	task->param = param;
+	task->status = PENDING;
+	task->result = NULL;
+	pthread_mutex_init(&task->lock, NULL);
+	
+	return (task);
 }
 
 /**
@@ -29,14 +29,16 @@ task_t *create_task(task_entry_t entry, void *param)
  */
 void destroy_task(task_t *task)
 {
-    if (!task)
-        return;
-
-    pthread_mutex_destroy(&task->lock);
-    if (task->result)
+	if (!task)
+		return;
+	
+	pthread_mutex_destroy(&task->lock);
+	
+	if (task->result)
         list_destroy(task->result, free);
-    free(task->result);
-    free(task);
+	
+	free(task->result);
+	free(task);
 }
 
 /**
@@ -46,31 +48,31 @@ void destroy_task(task_t *task)
  */
 void *exec_tasks(list_t const *tasks)
 {
-    node_t *node;
-    task_t *task;
-    int task_id = 0;
-
-    for (node = tasks->head; node; node = node->next)
-    {
-        task = node->content;
-        pthread_mutex_lock(&task->lock);
-
-        if (task->status == PENDING)
-        {
-            task->status = STARTED;
-            tprintf("[%02d] Started\n", task_id);
-            pthread_mutex_unlock(&task->lock);
-
-            task->result = task->entry(task->param);
-
-            pthread_mutex_lock(&task->lock);
-            task->status = task->result ? SUCCESS : FAILURE;
-            tprintf("[%02d] Success\n", task_id);
-        }
-        pthread_mutex_unlock(&task->lock);
-        task_id++;
+	node_t *node;
+	task_t *task;
+	int task_id = 0;
+	
+	for (node = tasks->head; node; node = node->next)
+	{
+		task = node->content;
+		pthread_mutex_lock(&task->lock);
+		
+		if (task->status == PENDING)
+		{
+			task->status = STARTED;
+			tprintf("[%02d] Started\n", task_id);
+			pthread_mutex_unlock(&task->lock);
+			
+			task->result = task->entry(task->param);
+			
+			pthread_mutex_lock(&task->lock);
+			task->status = task->result ? SUCCESS : FAILURE;
+			tprintf("[%02d] Success\n", task_id);
+		}
+		
+		pthread_mutex_unlock(&task->lock);
+		task_id++;
     }
-
-    return NULL;
+	return (NULL);
 }
 
