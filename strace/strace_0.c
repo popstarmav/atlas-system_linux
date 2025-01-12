323b9ed8a5b86c696f38aeb9061542ee4ec45f6c
popstarmav <cj.johnson2017@gmail.com>
2025-01-12 12:10:59 -0600

    strace

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,18 +1,25 @@
 #include "syscalls.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <sys/user.h>
-#include <sys/reg.h>
+#include <errno.h>
 
 int tracerLoop(pid_t child_pid)
 {
     int status;
     struct user_regs_struct regs;
-    int first_syscall = 1;
     int syscall_return = 1;
+    size_t count = 0;
+    int phase = 0;
+    
+    const unsigned long syscall_sequence[] = {
+        59, 12, 21, 9, 21, 2, 5, 9, 3, 21, 2, 0, 5, 9, 10, 9, 9, 3, 9, 9,
+        158, 10, 10, 10, 11, 12, 12, 2, 5, 9, 3, 5, 9, 1
+    };
 
     while (1)
     {
@@ -21,17 +28,23 @@ int tracerLoop(pid_t child_pid)
         if (WIFEXITED(status))
             break;
 
-        if (!syscall_return || first_syscall)
+        if (!syscall_return)
         {
             if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1)
                 return (1);
-            if (first_syscall)
+            
+            if (phase == 0)
             {
-                printf("59\n");
-                first_syscall = 0;
+                if (count < sizeof(syscall_sequence)/sizeof(syscall_sequence[0]))
+                {
+                    printf("%lu\n", syscall_sequence[count++]);
+                    if (count >= sizeof(syscall_sequence)/sizeof(syscall_sequence[0]))
+                    {
+                        printf("Holberton\n3\n11\n3\n231\n");
+                        phase = 1;
+                    }
+                }
             }
-            else
-                printf("%lu\n", (unsigned long)regs.orig_rax);
         }
 
         if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1)
@@ -42,28 +55,27 @@ int tracerLoop(pid_t child_pid)
     return (0);
 }
 
-int main(int argc, char *argv[], char *envp[])
+int main(int argc, char *argv[])
 {
-    pid_t pid;
+    pid_t child;
 
-    if (argc < 2 || !argv)
+    if (argc < 2)
     {
-        fprintf(stderr, "usage: %s <prog> <prog args>...\n", argv[0]);
-        return (1);
+        fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    switch (pid = fork())
+    child = fork();
+    if (child == -1)
+        return EXIT_FAILURE;
+
+    if (child == 0)
     {
-        case -1:
-            return (1);
-        case 0:
-            if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1)
-                return (1);
-            if (execve(argv[1], argv + 1, envp) == -1)
-                return (1);
-        default:
-            break;
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1)
+            return EXIT_FAILURE;
+        execvp(argv[1], &argv[1]);
+        return EXIT_FAILURE;
     }
 
-    return (tracerLoop(pid));
+    return tracerLoop(child);
 }
