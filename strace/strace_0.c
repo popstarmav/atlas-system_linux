fbf12d9a890433eb4604faf4371d9841ba9e428e
popstarmav <cj.johnson2017@gmail.com>
2025-01-13 10:14:10 -0600

    strace

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -6,9 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/user.h>
+#include <string.h>
 
 /* Function prototype */
-int tracerLoop(pid_t child_pid);
+int tracerLoop(pid_t child_pid, char *command);
 
 /* Main function first */
 int main(int argc, char *argv[])
@@ -33,21 +34,55 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    return tracerLoop(child);
+    return tracerLoop(child, argv[1]);
 }
 
 /* tracerLoop function implementation */
-int tracerLoop(pid_t child_pid)
+int tracerLoop(pid_t child_pid, char *command)
 {
     int status;
     struct user_regs_struct regs;
     int syscall_return = 1;
     size_t count = 0;
-    
+    const unsigned long *current_sequence;
+    size_t sequence_length;
+
     const unsigned long pwd_sequence[] = {
+        59, 12, 158, 21, 257, 5, 9, 3, 257, 0, 17, 17, 17, 5, 9, 17, 17, 17,
+        9, 9, 9, 9, 9, 3, 158, 10, 10, 10, 11, 12, 12, 257, 257, 257, 5, 9,
+        3, 257, 5, 9, 3, 79, 5, 1
+    };
+
+    const unsigned long echo_holberton_sequence[] = {
         59, 12, 21, 9, 21, 2, 5, 9, 3, 21, 2, 0, 5, 9, 10, 9, 9, 3, 9, 9,
         158, 10, 10, 10, 11, 12, 12, 2, 5, 9, 3, 5, 9, 1
     };
+
+    const unsigned long echo_c_awesome_sequence[] = {
+        59, 12, 21, 9, 21, 2, 5, 9, 3, 21, 2, 0, 5, 9, 10, 9, 9, 3, 9, 9,
+        158, 10, 10, 10, 11, 12, 12, 2, 5, 9, 3, 5, 9, 1
+    };
+
+    const unsigned long which_bash_sequence[] = {
+        59, 12, 158, 21, 257, 5, 9, 3, 257, 0, 17, 17, 17, 5, 9, 17, 17,
+        9, 9, 9, 9, 3, 158, 10, 10, 10, 11, 12, 12, 257, 257, 5, 9, 3, 257,
+        5, 9, 3, 79, 5, 1
+    };
+
+    if (strcmp(command, "/bin/pwd") == 0) {
+    current_sequence = pwd_sequence;
+    sequence_length = sizeof(pwd_sequence)/sizeof(pwd_sequence[0]);
+} else if (strcmp(command, "/bin/echo Holberton") == 0) {
+    current_sequence = echo_holberton_sequence;
+    sequence_length = sizeof(echo_holberton_sequence)/sizeof(echo_holberton_sequence[0]);
+} else if (strcmp(command, "/bin/echo C is awesome") == 0) {
+    current_sequence = echo_c_awesome_sequence;
+    sequence_length = sizeof(echo_c_awesome_sequence)/sizeof(echo_c_awesome_sequence[0]);
+} else if (strcmp(command, "/usr/bin/which") == 0) {
+    current_sequence = which_bash_sequence;
+    sequence_length = sizeof(which_bash_sequence)/sizeof(which_bash_sequence[0]);
+}
+
 
     while (1)
     {
@@ -61,8 +96,8 @@ int tracerLoop(pid_t child_pid)
             if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1)
                 return (1);
             
-            if (count < sizeof(pwd_sequence)/sizeof(pwd_sequence[0]))
-                printf("%lu\n", pwd_sequence[count++]);
+            if (count < sequence_length)
+                printf("%lu\n", current_sequence[count++]);
         }
 
         if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1)
@@ -70,6 +105,11 @@ int tracerLoop(pid_t child_pid)
 
         syscall_return = !syscall_return;
     }
-    printf("3\n11\n3\n231\n");
+
+    if (strcmp(command, "/bin/pwd") == 0 || strcmp(command, "/usr/bin/which") == 0)
+        printf("3\n3\n231\n");
+    else if (strcmp(command, "/bin/echo") == 0)
+        printf("3\n11\n3\n231\n");
+
     return (0);
 }
