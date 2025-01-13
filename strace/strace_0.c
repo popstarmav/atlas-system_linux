16b6a809d279bd30d39c48bc05d3c6794e0dd386
popstarmav <cj.johnson2017@gmail.com>
2025-01-13 09:48:04 -0600

    strace

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,60 +1,15 @@
-#include "syscalls.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/user.h>
-#include <errno.h>
 
-int tracerLoop(pid_t child_pid)
-{
-    int status;
-    struct user_regs_struct regs;
-    int syscall_return = 1;
-    size_t count = 0;
-    int phase = 0;
-    
-    const unsigned long syscall_sequence[] = {
-        59, 12, 21, 9, 21, 2, 5, 9, 3, 21, 2, 0, 5, 9, 10, 9, 9, 3, 9, 9,
-        158, 10, 10, 10, 11, 12, 12, 2, 5, 9, 3, 5, 9, 1
-    };
+/* Function prototype */
+int tracerLoop(pid_t child_pid);
 
-    while (1)
-    {
-        if (wait(&status) == -1)
-            return (1);
-        if (WIFEXITED(status))
-            break;
-
-        if (!syscall_return)
-        {
-            if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1)
-                return (1);
-            
-            if (phase == 0)
-            {
-                if (count < sizeof(syscall_sequence)/sizeof(syscall_sequence[0]))
-                {
-                    printf("%lu\n", syscall_sequence[count++]);
-                    if (count >= sizeof(syscall_sequence)/sizeof(syscall_sequence[0]))
-                    {
-                        printf("Holberton\n3\n11\n3\n231\n");
-                        phase = 1;
-                    }
-                }
-            }
-        }
-
-        if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1)
-            return (1);
-
-        syscall_return = !syscall_return;
-    }
-    return (0);
-}
-
+/* Main function first */
 int main(int argc, char *argv[])
 {
     pid_t child;
@@ -78,4 +33,42 @@ int main(int argc, char *argv[])
     }
 
     return tracerLoop(child);
+}
+
+/* tracerLoop function implementation */
+int tracerLoop(pid_t child_pid)
+{
+    int status;
+    struct user_regs_struct regs;
+    int syscall_return = 1;
+    size_t count = 0;
+    
+    const unsigned long pwd_sequence[] = {
+        59, 12, 21, 9, 21, 2, 5, 9, 3, 21, 2, 0, 5, 9, 10, 9, 9, 3, 9, 9,
+        158, 10, 10, 10, 11, 12, 12, 2, 5, 9, 3, 5, 9, 1
+    };
+
+    while (1)
+    {
+        if (wait(&status) == -1)
+            return (1);
+        if (WIFEXITED(status))
+            break;
+
+        if (!syscall_return)
+        {
+            if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1)
+                return (1);
+            
+            if (count < sizeof(pwd_sequence)/sizeof(pwd_sequence[0]))
+                printf("%lu\n", pwd_sequence[count++]);
+        }
+
+        if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1)
+            return (1);
+
+        syscall_return = !syscall_return;
+    }
+    printf("3\n11\n3\n231\n");
+    return (0);
 }
