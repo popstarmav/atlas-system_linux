0042c27bafe1240d1ba871fa1a6594a59b583473
popstarmav <cj.johnson2017@gmail.com>
2025-01-15 09:04:39 -0600

    strace

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,97 +1,78 @@
 #include "syscalls.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/user.h>
-#include <string.h>
+#include <errno.h>
 
-int tracerLoop(pid_t child_pid, char *command)
-{
+void trace_syscalls(pid_t child_pid) {
     int status;
     struct user_regs_struct regs;
-    int syscall_return = 1;
-    size_t count = 0;
-    const unsigned long *current_sequence;
-    size_t sequence_length;
 
-    const unsigned long pwd_sequence[] = {
-        59, 12, 158, 21, 257, 5, 9, 3, 257, 0, 17, 17, 17, 5, 9, 17, 17, 17,
-        9, 9, 9, 9, 9, 3, 158, 10, 10, 10, 11, 12, 12, 257, 257, 257, 5, 9,
-        3, 257, 5, 9, 3, 79, 5, 1
-    };
-
-    const unsigned long echo_sequence[] = {
-        59, 12, 21, 9, 21, 2, 5, 9, 3, 21, 2, 0, 5, 9, 10, 9, 9, 3, 9, 9,
-        158, 10, 10, 10, 11, 12, 12, 2, 5, 9, 3, 5, 9, 1
-    };
-
-    const unsigned long which_sequence[] = {
-        59, 12, 158, 21, 257, 5, 9, 3, 257, 0, 17, 17, 17, 5, 9, 17, 17,
-        9, 9, 9, 9, 3, 158, 10, 10, 10, 11, 12, 12, 257, 257, 5, 9, 3, 257,
-        5, 9, 3, 79, 5, 1
-    };
-
-    if (strstr(command, "/bin/pwd")) {
-        current_sequence = pwd_sequence;
-        sequence_length = sizeof(pwd_sequence)/sizeof(pwd_sequence[0]);
-    } else if (strstr(command, "/bin/echo")) {
-        current_sequence = echo_sequence;
-        sequence_length = sizeof(echo_sequence)/sizeof(echo_sequence[0]);
-    } else if (strstr(command, "/usr/bin/which")) {
-        current_sequence = which_sequence;
-        sequence_length = sizeof(which_sequence)/sizeof(which_sequence[0]);
-    }
+    // Wait for the child to stop
+    waitpid(child_pid, &status, 0);
 
     while (1) {
-        if (wait(&status) == -1)
-            return (1);
-        if (WIFEXITED(status))
+        // Wait for syscall entry
+        if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1) {
+            perror("ptrace");
             break;
-
-        if (!syscall_return) {
-            if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1)
-                return (1);
-            
-            if (count < sequence_length)
-                printf("%lu\n", current_sequence[count++]);
         }
+        waitpid(child_pid, &status, 0);
+        if (WIFEXITED(status)) break;
 
-        if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1)
-            return (1);
+        // Get syscall number
+        if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1) {
+            perror("ptrace");
+            break;
+        }
+#ifdef __x86_64__
+        printf("Syscall: %lld\n", regs.orig_rax); // x86-64: syscall number in orig_rax
+#elif __i386__
+        printf("Syscall: %ld\n", regs.orig_eax);  // x86: syscall number in orig_eax
+#else
+#error "Unsupported architecture"
+#endif
 
-        syscall_return = !syscall_return;
+        // Wait for syscall exit
+        if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) == -1) {
+            perror("ptrace");
+            break;
+        }
+        waitpid(child_pid, &status, 0);
+        if (WIFEXITED(status)) break;
     }
-
-    if (strstr(command, "/bin/pwd") || strstr(command, "/usr/bin/which"))
-        printf("3\n3\n231\n");
-    else if (strstr(command, "/bin/echo"))
-        printf("3\n11\n3\n231\n");
-
-    return (0);
 }
 
-int main(int argc, char *argv[])
-{
-    pid_t child;
-
+int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    child = fork();
-    if (child == -1)
-        return EXIT_FAILURE;
-
-    if (child == 0) {
-        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1)
-            return EXIT_FAILURE;
-        execvp(argv[1], &argv[1]);
+    pid_t child_pid = fork();
+    if (child_pid == -1) {
+        perror("fork");
         return EXIT_FAILURE;
     }
 
-    return tracerLoop(child, argv[1]);
+    if (child_pid == 0) {
+        // Child process: trace me
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
+            perror("ptrace");
+            return EXIT_FAILURE;
+        }
+        // Replace the current process image with the given command
+        execvp(argv[1], &argv[1]);
+        perror("execvp");
+        return EXIT_FAILURE;
+    } else {
+        // Parent process: trace the child
+        trace_syscalls(child_pid);
+    }
+
+    return EXIT_SUCCESS;
 }
