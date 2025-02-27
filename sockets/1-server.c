a798697b2950c9b2140f5136e9fbbf35d2688bbb
popstarmav <cj.johnson2017@gmail.com>
2025-02-26 22:06:54 -0600

    accept an entering connection

--- sockets/1-server.c
+++ sockets/1-server.c
@@ -7,54 +7,58 @@
 
 int main(void)
 {
-    int server_fd, client_fd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+	int server_fd, client_fd;
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t client_len = sizeof(client_addr);
 
-    /* Create socket */
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
-        perror("Failed to create socket");
-        exit(EXIT_FAILURE);
-    }
+	/* Create socket */
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		perror("Failed to create socket");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Configure server address */
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(12345);
+	/* Configure server address */
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(12345);
 
-    /* Bind socket */
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("Failed to bind");
-        exit(EXIT_FAILURE);
-    }
+	/* Bind socket */
+	if (bind(server_fd, (struct sockaddr *)
+	&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("Failed to bind");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Listen for connections */
-    if (listen(server_fd, 1) < 0)
-    {
-        perror("Failed to listen");
-        exit(EXIT_FAILURE);
-    }
+	/* Listen for connections */
+	if (listen(server_fd, 1) < 0)
+	{
+		perror("Failed to listen");
+		exit(EXIT_FAILURE);
+	}
 
-    printf("Server listening on port 12345\n");
+	printf("Server listening on port 12345\n");
 
-    /* Accept connection */
-    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-    if (client_fd < 0)
-    {
-        perror("Failed to accept connection");
-        exit(EXIT_FAILURE);
-    }
+	/* Accept connection */
+	client_fd = accept(server_fd, (struct sockaddr *)
+			&client_addr, &client_len);
 
-    /* Print client IP address */
-    printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+	if (client_fd < 0)
+	{
+		perror("Failed to accept connection");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Close connections */
-    close(client_fd);
-    close(server_fd);
+	/* Print client IP address */
+	printf("Client connected: %s\n", inet_ntoa
+			(client_addr.sin_addr));
 
-    return (0);
+	/* Close connections */
+	close(client_fd);
+	close(server_fd);
+
+	return (0);
 }
 
